3d0c4b544b77eb61b9bf69d55c50f66f95fd3b08
James Lamb <jaylamb20@gmail.com>
2018-03-24 18:47:40 -0500

    Got multiple components running in one page

---
+++ app/ui/frontend/src/components/ResumeForm.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <section class="section">
+    <br>
+    <h1>Resume entry form will go here</h1>
+    <br>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      salary_form: {
+        form_text: ""
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
