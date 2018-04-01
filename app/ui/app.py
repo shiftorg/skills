782554beddce38aa595151cd651df6aa4c09841c
James Lamb <james.lamb@uptake.com>
2018-04-01 02:34:23 -0500

    Connected parsed skills to rendered job cards

--- app/ui/app.py
+++ app/ui/app.py
@@ -51,7 +51,7 @@ def match_to_jobs():
         "job_name": "data scientist",
         "skills": {
             "has": input_skills,
-            "missing": []
+            "missing": ["x-ray vision"]
         }
     }]}
 
