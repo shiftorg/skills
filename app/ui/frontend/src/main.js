121f82808c5b4f20b529cda7aad513a831c043c5
Raghuram Pamidimarri <Raghu@Raghurams-MacBook-Pro.local>
2018-04-15 03:45:48 -0700

    Modals for individual job recommendations

--- app/ui/frontend/src/main.js
+++ app/ui/frontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
@@ -10,6 +11,7 @@ Vue.component('input-tag', InputTag);
 
 Vue.config.productionTip = false
 Vue.use(VueResource);
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
